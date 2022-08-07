cc0477eaab51549e058b0ae178456ec84ea60bc0
OAndreae <oliverandreae@outlook.com>
2022-08-07 20:07:10 +0100

    Initial commit.

---
+++ quizzler-flutter/lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_master.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+final quizMaster = QuizMaster();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> marks = [];
+
+  int questionNumber = 0;
+
+  Icon getMarkIcon(bool isCorrect) =>
+      isCorrect ? Icon(Icons.check, color: Colors.green) : Icon(Icons.close, color: Colors.red);
+
+  void evaluateAnswer(bool givenAnswer) async {
+    Icon icon = getMarkIcon(quizMaster.isCorrect(givenAnswer));
+    setState(() => marks.add(icon));
+
+    if (quizMaster.isFinished()) {
+      await Alert(
+          context: this.context,
+          title: 'Finished',
+          desc: 'Congratulations! You have completed the quiz.',
+          buttons: [
+            DialogButton(
+              child: Text('OKAY'),
+              onPressed: () => Navigator.pop(this.context),
+            )
+          ]).show();
+
+      setState(() {
+        quizMaster.reset();
+        marks.clear();
+      });
+    } else {
+      setState(() {
+        quizMaster.setNextQuestion();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizMaster.getCurrentQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: MaterialButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () => evaluateAnswer(true),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: MaterialButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () => evaluateAnswer(false),
+            ),
+          ),
+        ),
+        Row(
+          children: marks,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
